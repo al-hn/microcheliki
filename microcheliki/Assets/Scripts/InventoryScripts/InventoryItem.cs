9ee320115cf6f47d9fb97e6eb5d50366cde7e363
nurjwxx <160740578+nurjwxx@users.noreply.github.com>
2024-05-29 16:20:31 +0500

    Grid Inventory

---
+++ microcheliki/Assets/Scripts/InventoryScripts/InventoryItem.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class InventoryItem : MonoBehaviour , IBeginDragHandler , IDragHandler , IEndDragHandler
+{
+   [HideInInspector] public Item itm;
+   [Header("UI")]
+   public UnityEngine.UI.Image image;
+
+   public void InistialiseItem(Item newItem){
+      itm = newItem;
+      image.sprite = newItem.image;
+   }
+   [HideInInspector] public Transform parentAfterDrag;
+   public void OnBeginDrag(PointerEventData eventData){
+      image.raycastTarget = false;
+      parentAfterDrag = transform.parent;
+      transform.SetParent(transform.root);
+   }
+   public void OnDrag(PointerEventData eventData){
+      transform.position = Input.mousePosition;
+   }
+   public void OnEndDrag(PointerEventData eventData){
+      image.raycastTarget = true;
+      transform.SetParent(parentAfterDrag);
+
+   }
+}
